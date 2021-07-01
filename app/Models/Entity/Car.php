990aceaf2e7e0999459c4a8dc3313453f8ca9e58
euler <euler@LAPTOP-7SGNAHRQ.localdomain>
2021-07-01 14:30:34 +0200

    Updated entities app

--- app/Models/Entity/Car.php
+++ app/Models/Entity/Car.php
@@ -14,21 +14,23 @@ class Car
 
     private $tank;
 
-    // public function __construct(Tank $tank)   // EUREKA implementation new method construct
-    // {
-    //     $this->tank = $tank;
-    // }
+    private $oldTank;
+
+    public function __construct(Tank $tank)   // EUREKA implementation new method construct
+    {
+        $this->tank = $tank;
+    }
 
     public function fillTank(FillingOrder $fillingOrder): FillingOrder
     {   
-        $this->tank = new Tank;
+        //$this->tank = new Tank;
         $fillingOrder->fill($this->tank);
         return $fillingOrder;
     }
 
     public function changeTank(ChangeTankOrder $changeTankOrder, Mechanic $mechanic): void
     {
-        $oldTank = $this->tank;
+        $this->oldTank = $this->tank;
         $this->tank = $changeTankOrder->fetchTank($mechanic);
         // StaticEventPublisher::getInstance()->record(
         // );
--- app/Models/Entity/ChangeTankOrder.php
+++ app/Models/Entity/ChangeTankOrder.php
@@ -3,11 +3,18 @@ namespace App\Models\Entity;
 
 use App\Models\Entity\Mechanic;
 
+use App\Models\Entity\Tank;
+
 class ChangeTankOrder
 {
     private $tank;
 
     private $executedBy;
+
+    public function __construct(Tank $tank)
+    {
+        $this->tank = $tank;
+    }
 
     public function fetchTank(Mechanic $executedBy)
     {
